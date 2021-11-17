a297d7b2ff7bef5ef4720dc8086ca56127b03e50
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2021-11-17 12:02:34 +0100

    remove echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,4 @@ ls
 mkdir -p ~/colcon_ws/src
 mv $2 ~/colcon_ws/src
 cd ~/colcon_ws
-workspace = "colcon_ws"
-echo "::set-output name=ros-workspace-directory-name::$workspace"
 source "/opt/ros/$1/setup.bash" && colcon build --symlink-install
