c18a915e2ca00ae4abf75a96b435c2be0090cd20
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2021-11-17 11:26:06 +0100

    testing entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 # setup ros2 environment
 source "/opt/ros/$1/setup.bash"
+ls
 mkdir -p ~/colcon_ws/src
 mv $2 ~/colcon_ws/src
 cd ~/colcon_ws
 source "/opt/ros/$1/setup.bash" && colcon build --symlink-install
-exec "$@"
